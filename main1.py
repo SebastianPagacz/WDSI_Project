e5d987fd5f43b763a3373fbfe6f4795eb262ed3c
SebastianPagacz <234595@student.uek.krakow.pl>
2024-05-11 13:28:09 +0200

    wieksza automatyzacja

--- main1.py
+++ main1.py
@@ -16,13 +16,19 @@ class Product:
         self.prod_final_time = prod_final_time
 
     def raport(self):
+        raport_res =  f"Product name: {self.prod_name} \nProduct stock: {self.prod_stock} \nProduct demand: {self.prod_demand} \n"
         if self.prod_stock - self.prod_demand > 0:
-            return f"Product name: {self.prod_name} \nProduct stock: {self.prod_stock} \nProduct demand: {self.prod_demand} \nWe have enough stock to complete. \nStock after completing the order: {self.prod_stock - self.prod_demand}. \nComponents: \n{self.prod_components_amount[0].comp_name} \n{self.prod_components_amount[1].comp_name} \n{self.prod_components_amount[2].comp_name}"
+            raport_res += f"We have enough stock to complete. \nStock after completing the order: {self.prod_stock - self.prod_demand}. \nComponents: \n"
+            for x in range(len(self.prod_components_amount)):
+                raport_res +=f"{self.prod_components_amount[x].comp_name} \n"
+            return raport_res[0:-1]
         else:
-            print(f"Product name: {self.prod_name} \nProduct stock: {self.prod_stock} \nProduct demand: {self.prod_demand} \nWe dont have enough stock. \nStock that we need to complete: {abs(self.prod_stock - self.prod_demand)}. \nComponents: {self.prod_components_amount[0]}")
-            
+            raport_res += f"We dont have enough stock. \nStock that we need to complete: {abs(self.prod_stock - self.prod_demand)}. \nComponents: \n"
+            for x in range(len(self.prod_components_amount)):
+                raport_res +=f"{self.prod_components_amount[x].comp_name} \n"
+            return raport_res[0:-1]
+        
     def obliczenia(self):
-        control_value = 0
         demand_component = []
         if self.prod_demand > self.prod_stock:
             for x in range(len(self.prod_components_amount)):
@@ -33,20 +39,21 @@ class Product:
         if sum(demand_component) == 0:
             return "we have everything in stock"
         else:    
-            #do zmiany, wrzucic petle zalezna od ilosci komponentow
-            control_value = 1
-            print(f"We need: \n{self.prod_components_amount[0].comp_name}: {abs(self.prod_components_amount[0].comp_stock - (self.prod_components_amount[0].comp_to_make * self.prod_demand))} pieces \n{self.prod_components_amount[1].comp_name}: {abs(self.prod_components_amount[1].comp_stock - (self.prod_components_amount[1].comp_to_make * self.prod_demand))} pieces \n{self.prod_components_amount[2].comp_name}: {abs(self.prod_components_amount[2].comp_stock - (self.prod_components_amount[2].comp_to_make * self.prod_demand))} pieces \n")
-            #zmienic to kurestwo na petle xdddd
-            if control_value == 1:
+            res_obliczenia = "We need: \n"
+            for x in range(len(self.prod_components_amount)):
+                res_obliczenia += f"{self.prod_components_amount[x].comp_name}: {abs(self.prod_components_amount[x].comp_stock - (self.prod_components_amount[x].comp_to_make * self.prod_demand))} pieces \n"
                 comp1_order = self.prod_final_time - (self.prod_components_amount[0].comp_order_time + self.prod_components_amount[0].comp_time_to_make + 1)
-                return f"Order {self.prod_components_amount[0].comp_name} in: {comp1_order}"
-
+            res_obliczenia += "\nOrder: \n"
+            for x in range(len(self.prod_components_amount)):
+                res_obliczenia += f"{self.prod_components_amount[x].comp_name} in: {comp1_order} days\n" 
+            return res_obliczenia
+                
 ciasto = Component("Ciasto", 30, 3, 2, 1)
 polewa = Component("Polewa", 50, 1, 1, 2)
 wisienka = Component("Wisienka", 25, 2, 0, 1)
 
 
-tort = Product("Tort", 1, 50, 1, [ciasto, wisienka, polewa], 7)
+tort = Product("Tort", 1, 5, 1, [ciasto, wisienka, polewa], 10)
 
-print(tort.obliczenia())
-#print(tort.raport())
+print(tort.raport())
+#print(tort.obliczenia())
