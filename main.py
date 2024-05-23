9a16362571ba38573aca2fff736150e69d8bbaa0
SebastianPagacz <149250401+SebastianPagacz@users.noreply.github.com>
2024-05-23 20:30:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from main1 import Component, Product
 
 #Component(name, stock, order_time, time_to_make, amount_needed_in_product)
+#Component class takes 5 arguments, all of them are required, to create an object
+#First argument is name, it should be a string value
+#Second is stock, which should be a positive inteeger
+#Third is order_time, also should be a positive inteeger
+#Fourth is time_to_make, also a positive inteeger
+#The last one from Component class is amount_needed_in_product, it also should be a positive inteeger number
+
 #Product(name, stock, demand, time_to_make, components_amount, final_time)
+#Product class takes 6 positional arguments, all which are required
+#First argument is name, it should be a string value
+#Second is stock, which should be a positive inteeger
+#Third is demand, also should be a positive inteeger
+#Fourth is time_to_make, also a positive inteeger
+#The fith one components_amount, for the code to work properly it has to be list of strings
+#The last one is final_time, this argument has to be positive inteeger
 
-ciasto = Component("Ciasto", 3, 3, 2, 1)
-polewa = Component("Polewa", 5, 1, 1, 2)
-wisienka = Component("Wisienka", 2, 2, 0, 1)
-
-
-tort = Product("Tort", 1, 50, 1, [ciasto, wisienka, polewa], 1)
-
-#Returns report about stock and gives us information about product and components
+#Returns a string that contains ifnroamtion
 #print(tort.raport())
 #Calculates amounts and time within we have to place order for components
 #print(tort.obliczenia())
